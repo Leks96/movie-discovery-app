251cc4f659488e9ba791079c50ac4d94e6a8e5a5
root <root@DESKTOP-DHRCVPT>
2023-09-19 08:14:13 +0100

    added some testid's

--- src/component/movieCard.js
+++ src/component/movieCard.js
@@ -8,16 +8,15 @@ const MovieCard = (props) => {
 
   return (
     <div>
-        <div className='movie-card'>
+        <div className='movie-card' data-testid='movie-card'>
             <Link to={`/movie/${props.id}`}>
-            <div className='poster'>
+            <div className='poster' data-testid='movie-poster'>
                 <img src={IMG + props.poster_path} alt='movie poster'/>
             </div>
 
             <div className='info'>
-                <p className='title'>{props.title}</p>
-                <p className='release-date'>{props.release_date}</p>
-                <p className='votes'>{props.vote_average}</p>
+                <p className='title' data-testid='movie-title'>{props.title}</p>
+                <p className='release-date' data-testid='movie-release-date'>{props.release_date}</p>
             </div>
             </Link>
         </div>
